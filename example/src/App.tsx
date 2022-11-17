45b0b9af93acb1d0aab0b9bdd1f1d70a6b2e3c33
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-17 14:58:37 +0100

    Add a basic 80's UI

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import { generate, getPublicKey, multiply } from '@pagopa/io-react-native-crypto';
 
 export default function App() {
@@ -8,32 +8,85 @@ export default function App() {
 
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
-    getPublicKey("ec").then((value) => {
-      console.log(`${JSON.stringify(value)}`);
-    })
-    .catch((reason) => {
-      console.log(reason);
-    })
-    getPublicKey("rsa").then((value) => {
-      console.log(`${JSON.stringify(value)}`);
-    })
-    .catch((reason) => {
-      console.log(reason);
-    })
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, padding: 20, backgroundColor: "#ccc" }}>
+      <View style={{
+        flex: 1
+      }}>
+        <Text style={{ fontWeight: "bold", height: "auto" }}>Ket tag: </Text>
+        <TextInput
+          style={{
+            marginVertical: 8,
+            height: 40,
+            borderColor: "black",
+            borderWidth: 1,
+            borderRadius: 10
+          }}
+          placeholder="key tag"
+        />
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between"          
+        }}>
+          <Button
+            title="get"
+            color="#FF0000"
+            onPress={() => {
+    getPublicKey("ec").then((value) => {
+      console.log(`${JSON.stringify(value)}`);
+    })
+      .catch((reason) => {
+        console.log(reason);
+      })
+    getPublicKey("rsa").then((value) => {
+      console.log(`${JSON.stringify(value)}`);
+    })
+      .catch((reason) => {
+        console.log(reason);
+      })
+            }} />
+          <Button
+            title="create"
+            color="#FF0CCF"
+            onPress={() => {
+
+    generate("ec").then((value) => {
+      console.log(`${JSON.stringify(value)}`);
+    })
+      .catch((reason) => {
+        console.log(reason);
+      })
+    generate("rsa").then((value) => {
+      console.log(`${JSON.stringify(value)}`);
+    })
+      .catch((reason) => {
+        console.log(reason);
+      })
+            }} />
+          <Button
+            title="delete"
+            color="#0000FF"
+            onPress={() => {
+
+            }} />
+        </View>
+        <Text style={{
+          marginVertical: 16,
+          flexGrow: 1,
+          padding: 8,
+          backgroundColor: "#CCDDCC"
+        }}></Text>
+      </View>
+      <Text style={{backgroundColor:"#fff"}}>Result: {result}</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
   box: {
     width: 60,
